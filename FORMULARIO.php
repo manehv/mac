fc26db101ec4c4e7c40c3fa6c9dccb9c059909ca
Mac Center Dev Server <git@8manos.com>
2014-06-24 11:48:09 -0500

    first commit pre-woocommerce

---
+++ FORMULARIO.php
@@ -0,0 +1,3 @@
+<form style="color:red; display: inline-block !important; float:left; " method="post" action="pagina2.php"> 
+Nombre:<input type="text" name="nombre">  Email:<input type="text" name="edad"><input type="submit" value="ENVIAR"> 
+</form>
