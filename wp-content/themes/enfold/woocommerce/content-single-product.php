2b449e09a99cf9a41b983c6fb1008010d5c17fe4
Atoshi <atoshi@runwalsoft.com>
2014-07-04 16:15:03 +0530

    variations

--- wp-content/themes/enfold/woocommerce/content-single-product.php
+++ wp-content/themes/enfold/woocommerce/content-single-product.php
@@ -43,7 +43,9 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 	    </div>
 	</div>
     </div>
-   <?php woocommerce_output_related_products();//for related products ?>
+    <div class="col-lg-9">
+	<?php woocommerce_output_related_products();//for related products ?> 
+    </div>
 </div>
 
 <div itemscope itemtype="<?php echo woocommerce_get_product_schema(); ?>" id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -55,9 +57,9 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 		 * @hooked woocommerce_show_product_sale_flash - 10
 		 * @hooked woocommerce_show_product_images - 20
 		 */
-	//	do_action('woocommerce_before_single_product_summary' );
+		do_action( 'woocommerce_before_single_product_summary' );
 	?>
-	
+
 	<div class="summary entry-summary">
 
 		<?php
@@ -72,7 +74,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 			 * @hooked woocommerce_template_single_meta - 40
 			 * @hooked woocommerce_template_single_sharing - 50
 			 */
-		
+			do_action( 'woocommerce_single_product_summary' );
 		?>
 
 	</div><!-- .summary -->
@@ -84,7 +86,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 		 * @hooked woocommerce_output_product_data_tabs - 10
 		 * @hooked woocommerce_output_related_products - 20
 		 */
-	//do_action( 'woocommerce_after_single_product_summary' );
+		do_action( 'woocommerce_after_single_product_summary' );
 	?>
 
 	<meta itemprop="url" content="<?php the_permalink(); ?>" />
