ff0b57fe7e960baedc02db42f1e370fc19f4f88f
somnath <somnath@runwalsoft.com>
2014-07-08 15:17:04 +0530

    js model work
    Signed-off-by: somnath <somnath@runwalsoft.com>

--- wp-content/themes/enfold/js/custom_variation.js
+++ wp-content/themes/enfold/js/custom_variation.js
@@ -11,8 +11,11 @@ jQuery(function()
 		var variation_id=jQuery(this).attr('variation_id');
 		jQuery('.clshide').hide();
 		jQuery('.'+variation_id).show();
-		
+		jQuery('.model').children().removeClass('active');
+		jQuery('.model').find('.'+variation_id).find('.variation').addClass('active');
+	  jQuery('.clsVariation	').find('[variation_id='+variation_id+']').addClass('active');
 		
 	});
+	
 
 });
