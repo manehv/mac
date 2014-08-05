c3a5e5356aee5a40709b949cd002629d2889a238
Atoshi <atoshi@runwalsoft.com>
2014-08-05 13:00:51 +0530

    js for sticky sidebar

--- wp-content/themes/enfold/js/custom.js
+++ wp-content/themes/enfold/js/custom.js
@@ -1,10 +1,22 @@
 jQuery(document).ready(function($)
 {
-	$( window ).resize(function() {
+	/*$( window ).resize(function() {
 		if ( $(window).width() > 979 )
-			$.lockfixed("#idSticky",{offset: {top: 100, bottom: 70}});
+			$.lockfixed("#idSticky",{offset: {top: 150, bottom: 70}});
 		else	
 		$( "#idSticky" ).unbind('scroll resize orientationchange load lockfixed:pageupdate');
-		
-	});
+	});*/
+	
+	function checkWidth() {
+        var windowSize = $(window).width();
+
+        if (windowSize <= 979) {
+           $( "#idSticky" ).unbind('scroll resize orientationchange load lockfixed:pageupdate');
+        }
+        else {
+            $.lockfixed("#idSticky",{offset: {top: 150, bottom: 70}});
+        }
+    }
+    checkWidth();
+    $(window).resize(checkWidth);
 });
