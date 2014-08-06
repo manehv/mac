5cf9d5887e232b5728ad00adcbbd6477a5f906cc
somnath <somnath@runwalsoft.com>
2014-08-06 18:05:57 +0530

    solve sticky side bar
    Signed-off-by: somnath <somnath@runwalsoft.com>

--- wp-content/themes/enfold/js/custom.js
+++ wp-content/themes/enfold/js/custom.js
@@ -1,11 +1,45 @@
 jQuery(document).ready(function($)
 {
-	/*$( window ).resize(function() {
-		if ( $(window).width() > 979 )
-			$.lockfixed("#idSticky",{offset: {top: 150, bottom: 70}});
-		else	
-		$( "#idSticky" ).unbind('scroll resize orientationchange load lockfixed:pageupdate');
-	});*/
+	
+	
+	$.lockfixed("#idSticky",{offset: {top: 150, bottom: 114}});
+		 
+/*		if (!!$('#idSticky').offset()) 
+		{
+ 
+				var stickyTop = $('#idSticky').offset().top; // returns number
+		
+		  
+				$(window).scroll(function(){ // scroll event
+		
+		 		var windowTop = $(window).scrollTop(); // returns number
+		
+			 if ( $(window).width() > 979 )
+			{	
+				
+					if (stickyTop < windowTop){
+				
+						$('#idSticky').css({ position: 'fixed', top: 0 });
+					}
+					else {
+				
+						$('#idSticky').css('position','static');
+					}
+			}		
+		
+				});
+	  }
+	
+	*/ 
+	
+	/*	$.lockfixed("#idSticky",{offset: {top: 150, bottom: 70}});
+	
+	$( window ).resize(function() {
+	if ( $(window).width() > 979 )
+		$.lockfixed("#idSticky",{offset: {top: 150, bottom: 70}});
+	else	
+	  $( "#idSticky" ).unbind('scroll resize orientationchange load lockfixed:pageupdate');
+ });
 	
 	function checkWidth() {
         var windowSize = $(window).width();
@@ -18,5 +52,5 @@ jQuery(document).ready(function($)
         }
     }
     checkWidth();
-    $(window).resize(checkWidth);
+    $(window).resize(checkWidth);*/
 });
