8f6da96eeee55a6c88eacd588ca1271ff10f3fce
Manish S Runwal <manish@runwalsoft.com>
2014-09-26 11:12:32 +0530

    Css Adjustment and form pattern.

--- wp-content/themes/enfold/woocommerce/single-product/add-to-cart/variable.php
+++ wp-content/themes/enfold/woocommerce/single-product/add-to-cart/variable.php
@@ -20,36 +20,35 @@ global $woocommerce, $product, $post;
 
 <form class="variations_form cart" method="post" enctype='multipart/form-data' data-product_id="<?php echo $post->ID; ?>" data-product_variations="<?php echo esc_attr( json_encode( $available_variations ) ) ?>">
 	<?php if ( ! empty( $available_variations ) ) : ?>
-		<table class="variations" cellspacing="0">
+		<table class="variations" cellspacing="0" cellpadding="0">
 			<tbody>
 				<?php $loop = 0; foreach ( $attributes as $name => $options ) : $loop++; ?>
 					<tr>
-						<td class="label"><label for="<?php echo sanitize_title($name); ?>"><?php echo wc_attribute_label( $name ); ?></label></td>
+						<!--<td class="label"><label for="<?php echo sanitize_title($name); ?>"><?php echo wc_attribute_label( $name ); ?></label></td> -->
 						<td class="value"><fieldset>
-                        <strong>Choose An Option...</strong><br />
-                        <?php
-                            if ( is_array( $options ) ) {
- 
-                                if ( empty( $_POST ) )
-                                    $selected_value = ( isset( $selected_attributes[ sanitize_title( $name ) ] ) ) ? $selected_attributes[ sanitize_title( $name ) ] : '';
-                                else
-                                    $selected_value = isset( $_POST[ 'attribute_' . sanitize_title( $name ) ] ) ? $_POST[ 'attribute_' . sanitize_title( $name ) ] : '';
-								//echo   $selected_value;
-                                // Get terms if this is a taxonomy - ordered
-                                if ( taxonomy_exists( sanitize_title( $name ) ) ) {
- 
-                                    $terms = get_terms( sanitize_title($name), array('menu_order' => 'ASC') );
-									
-                                    foreach ( $terms as $term ) {
-                                        if ( ! in_array( $term->slug, $options ) ) continue;
-                                        echo '<input type="radio" value="' . strtolower($term->slug) . '" ' . checked( strtolower ($selected_value), strtolower ($term->slug), false ) . ' id="'. esc_attr( sanitize_title($name) ) .'" name="attribute_'. sanitize_title($name).'">' . apply_filters( 'woocommerce_variation_option_name', $term->name ).'<br />';
-                                    }
-                                } else {
-                                    foreach ( $options as $option )
-                                        echo '<input type="radio" value="' .esc_attr( sanitize_title( $option ) ) . '" ' . checked( sanitize_title( $selected_value ), sanitize_title( $option ), false ) . ' id="'. esc_attr( sanitize_title($name) ) .'" name="attribute_'. sanitize_title($name).'">' . apply_filters( 'woocommerce_variation_option_name', $option ) . '<br />';
-                                }
-                            }
-                        ?>
+							<?php
+									if ( is_array( $options ) ) {
+												echo '<h2 class="clsFinish">'. sanitize_title($name). ': </h2>';
+											if ( empty( $_POST ) )
+													$selected_value = ( isset( $selected_attributes[ sanitize_title( $name ) ] ) ) ? $selected_attributes[ sanitize_title( $name ) ] : '';
+											else
+													$selected_value = isset( $_POST[ 'attribute_' . sanitize_title( $name ) ] ) ? $_POST[ 'attribute_' . sanitize_title( $name ) ] : '';
+			//echo   $selected_value;
+											// Get terms if this is a taxonomy - ordered
+											if ( taxonomy_exists( sanitize_title( $name ) ) ) {
+
+													$terms = get_terms( sanitize_title($name), array('menu_order' => 'ASC') );
+				
+													foreach ( $terms as $term ) {
+															if ( ! in_array( $term->slug, $options ) ) continue;
+															echo '<input type="radio" value="' . strtolower($term->slug) . '" ' . checked( strtolower ($selected_value), strtolower ($term->slug), false ) . ' id="'. esc_attr( sanitize_title($name) ) .'" name="attribute_'. sanitize_title($name).'">' . apply_filters( 'woocommerce_variation_option_name', $term->name ).'<br />';
+													}
+											} else {
+													foreach ( $options as $option )
+															echo '<input type="radio" value="' .esc_attr( sanitize_title( $option ) ) . '" ' . checked( sanitize_title( $selected_value ), sanitize_title( $option ), false ) . ' id="'. esc_attr( sanitize_title($name) ) .'" name="attribute_'. sanitize_title($name).'">' . apply_filters( 'woocommerce_variation_option_name', $option ) . '<br />';
+											}
+									}
+							?>
                     </fieldset> <?php
 							if ( sizeof($attributes) == $loop )
 								//echo '<a class="reset_variations" href="#reset">' . __( 'Clear selection', 'woocommerce' ) . '</a>';
@@ -58,7 +57,8 @@ global $woocommerce, $product, $post;
 		        <?php endforeach;?>
 			</tbody>
 		</table>
-
+	<?php endif; ?>
+	<?php if ( ! empty( $available_variations ) ) : ?>
 		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
 		<div class="single_variation_wrap" style="display:none;">
