4b4f707bf0fb71f516373d278fdfe3b9294bff5e
Anabelle Handdoek <github@huellaspyp.com>
2014-12-30 12:31:52 -0500

    cleanup

--- wp-content/themes/enfold/woocommerce/content-single-product.php
+++ wp-content/themes/enfold/woocommerce/content-single-product.php
@@ -33,7 +33,7 @@ global $post, $woocommerce, $product;
 	<div class="row">
 		<div class="col-md-9 clsContent">
 			<h1 class="clsBotTitle">
-				<?php _e('Choose a ','avia_framework'). the_title(); ?>
+				<?php the_title(); ?>
 			</h1>
 			<div class='row'>
 				<?php if($product->product_type == "variable"): ?>
