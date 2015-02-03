ebbfe3761e76ae0161734d3853b12fd91c3e6a60
Anabelle Handdoek <github@huellaspyp.com>
2015-02-03 08:40:39 -0500

    my account copy

--- wp-content/themes/enfold/woocommerce/myaccount/my-account.php
+++ wp-content/themes/enfold/woocommerce/myaccount/my-account.php
@@ -14,17 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 wc_print_notices(); ?>
 <h2 class='clsMyAcc'><?php _e('My Shopping','avia_framework'); ?></h2>
 <p class="myaccount_user">
-	<?php
-	printf(
-		__( 'Hello <strong>%1$s</strong> (not %1$s? <a href="%2$s">Sign out</a>).', 'woocommerce' ) . ' ',
-		$current_user->display_name,
-		wp_logout_url( get_permalink( wc_get_page_id( 'myaccount' ) ) )
-	);
-
-	printf( __( 'From your account dashboard you can view your recent orders, manage your shipping and billing addresses and <a href="%s">edit your password and account details</a>.', 'woocommerce' ),
-		wc_customer_edit_account_url()
-	);
-	?>
+	Desde la página de cuenta puedes ver pedidos recientes, gestionar tu dirección de envío, dirección de facturación y cambiar tu contraseña.
 </p>
 
 <?php do_action( 'woocommerce_before_my_account' ); ?>
