ace30d032ac91c067f31d9126d1ef53b0e140666
Manish S Runwal <manish@runwalsoft.com>
2015-02-24 23:24:31 +0530

    No message shown if product count is less than 0

--- wp-content/themes/enfold/config-woocommerce/config.php
+++ wp-content/themes/enfold/config-woocommerce/config.php
@@ -789,7 +789,10 @@ function avia_woocommerce_cart_dropdown()
 	$output = "";
 	$output .= "<ul class = 'cart_dropdown' data-success='".__('was added to the cart', 'avia_framework')."'>
 							<li class='cart_dropdown_first'>"  ;
+	if($qty > 0)
 	$output .= "<a class='cart_dropdown_link' href='".$link."'><span class='desc'>" . __('Products in cart','avia_framework') .':'. $qty ."</span> <span ".av_icon_string('cart')."></span></a><!--<span class='cart_subtotal'>".$cart_subtotal."</span>-->";
+	else 
+	$output .= "<a class='cart_dropdown_link' href='".$link."'><span ".av_icon_string('cart')."></span></a><!--<span class='cart_subtotal'>".$cart_subtotal."</span>-->";
 	$output .= "<div class='dropdown_widget dropdown_widget_cart'><div class='avia-arrow'></div>";
 	$output .= '<div class="widget_shopping_cart_content"></div>';
 	$output .= "</div>";
