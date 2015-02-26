63f4e47d2d656414849e2ebb4ce19b33e3742c84
manuelhv <manuelhv@users.noreply.github.com>
2015-02-26 15:16:46 -0500

    Update search.php

--- wp-content/themes/enfold/search.php
+++ wp-content/themes/enfold/search.php
@@ -8,6 +8,7 @@ $results = avia_which_archive();
 echo avia_title(array('title' => $results ));
 ?>
 
+<div style="background-color:#d7d7d7 !important">
 <div class='container_wrap container_wrap_first main_color <?php avia_layout_class( 'main' ); ?>'>
 <div class='container'>
 <main class='content template-search <?php avia_layout_class( 'content' ); ?> units' <?php avia_markup_helper(array('context' => 'content'));?>>
@@ -65,6 +66,14 @@ get_sidebar();
 ?>
 </div><!--end container-->
 </div><!-- close default .container_wrap element -->
-
+</div>
 <?php echo do_shortcode("[av_hr class='full' height='50' shadow='no-shadow' position='center']"); ?>
+[av_hr class='invisible' height='20' shadow='no-shadow' position='center']
+
+<?php echo do_shortcode("[av_partner columns='3' heading='' size='featured_large' border='' type='slider' animation='slide' navigation='no' autoplay='false' interval='5']
+[av_partner_logo id='11661' attachment='' hover='' link='manually,http://mc.arbolnaranja.com/mac/' linktitle='' link_target='']
+[av_partner_logo id='8550' attachment='' hover='' link='manually,http://mc.arbolnaranja.com/accesorios/' linktitle='' link_target='']
+[av_partner_logo id='13275' attachment='' hover='' link='manually,http://mc.arbolnaranja.com/training-room/' linktitle='' link_target='']
+[/av_partner]");?>
+
 <?php get_footer(); ?>
