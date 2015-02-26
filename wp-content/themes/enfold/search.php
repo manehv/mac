4784998730ffd18533b753e765e094d0bad10119
manuelhv <manuelhv@users.noreply.github.com>
2015-02-26 14:52:48 -0500

    Update search.php

--- wp-content/themes/enfold/search.php
+++ wp-content/themes/enfold/search.php
@@ -13,37 +13,28 @@ echo avia_title(array('title' => $results ));
 <div class='page-heading-container clearfix'>
 <section class="search_form_field">
 <?php echo do_shortcode("[av_hr class='invisible' height='100' shadow='no-shadow' position='center']
-
 [av_one_third first]
 [av_textblock]
 <b>
 <p style='font-size: 45px; text-align: right; font-weight: 900; color: #5d5d5d;  line-height: 50px'>Resultados de tu búsqueda</p>
-
 </b>
 [/av_textblock]
 [/av_one_third]
-
 [av_one_fifth]
 [av_textblock ]
 <div style='border-left: 2px solid #ccc; width:3px; height:150px; margin:auto'></div>
 [/av_textblock]
 [/av_one_fifth]
-
 [av_one_third]
-[av_hr class='invisible' height='50' shadow='no-shadow' position='center']
-
+[av_hr class='invisible' height='20' shadow='no-shadow' position='center']
 [av_textblock]
 Si no obtuviste los resultados esperados, prueba una nueva búsqueda con diferentes términos.
 [/av_textblock]
 [/av_one_third]
-
-
-
 [av_one_half first]
-
 [/av_one_half]")?> 
 
-<div style="width:170px; float: right; margin-right: 27% ;margin-top:-150px; z-index:8000"> 
+<div style="width:170px; float: right; margin-right: 27% ;margin-top:-180px; z-index:8000"> 
 <?php
 get_search_form();
 ?>
