089c7e05935e7ab1d1b9d66b23980733554d7c19
manuelhv <manuelhv@users.noreply.github.com>
2015-02-26 13:54:45 -0500

    Update search.php

--- wp-content/themes/enfold/search.php
+++ wp-content/themes/enfold/search.php
@@ -34,16 +34,17 @@ echo avia_title(array('title' => $results ));
 
 [av_textblock]
 Si no obtuviste los resultados esperados, prueba una nueva búsqueda con diferentes términos.
+<?php
+get_search_form();
+?>  
 [/av_textblock]
 
 [/av_one_third]
+
 [av_hr class='default' height='50' shadow='no-shadow' position='center']
 ")?> 
     
-<?php
-get_search_form();
-?>   
-      
+    
 </section>
 </div>
 <div style="min-height:250px">
