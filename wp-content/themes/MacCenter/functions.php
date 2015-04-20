ddfc5f9896b9b6d62feee58e98cc94ae0dedc8b9
Anabelle Handdoek <github@huellaspyp.com>
2015-04-20 10:49:26 -0500

    Hide standard shipping option when free shipping is available

--- wp-content/themes/MacCenter/functions.php
+++ wp-content/themes/MacCenter/functions.php
@@ -33,23 +33,33 @@ function wcs_stock_text_shop_page() {
     // }
 }
 
-// Hide standard shipping option when free shipping is available
-add_filter( 'woocommerce_available_shipping_methods', 'hide_standard_shipping_when_free_is_available' , 10, 1 );
-
 /**
-* Hide Standard Shipping option when free shipping is available
-*
-* @param array $available_methods
-*/
-function hide_standard_shipping_when_free_is_available( $available_methods ) {
-
-    if( isset( $available_methods['free_shipping'] ) AND isset( $available_methods['flat_rate'] ) ) {
-
-        // remove standard shipping option
-        unset( $available_methods['flat_rate'] );
+ * woocommerce_package_rates is a 2.1+ hook
+ */
+add_filter( 'woocommerce_package_rates', 'hide_shipping_when_free_is_available', 10, 2 );
+ 
+/**
+ * Hide shipping rates when free shipping is available
+ *
+ * @param array $rates Array of rates found for the package
+ * @param array $package The package array/object being shipped
+ * @return array of modified rates
+ */
+function hide_shipping_when_free_is_available( $rates, $package ) {
+    
+    // Only modify rates if free_shipping is present
+    if ( isset( $rates['free_shipping'] ) ) {
+    
+        // To unset a single rate/method, do the following. This example unsets flat_rate shipping
+        unset( $rates['flat_rate'] );
+        
+        // To unset all methods except for free_shipping, do the following
+        $free_shipping          = $rates['free_shipping'];
+        $rates                  = array();
+        $rates['free_shipping'] = $free_shipping;
     }
-
-    return $available_methods;
+    
+    return $rates;
 }
 
 //this function determines which version of the registration to call
