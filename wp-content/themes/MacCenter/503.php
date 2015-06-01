ffd2721b65ccc9a6c9a6517e7660dc609f9cd2e0
manuelhv <manuelhv@users.noreply.github.com>
2015-06-01 18:12:42 -0500

    Update 503.php

--- wp-content/themes/MacCenter/503.php
+++ wp-content/themes/MacCenter/503.php
@@ -1,220 +1,205 @@
 <!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es-CO">
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 
-	<title>Pronto estaremos de vuelta</title>
-	<style type="text/css">
-		
-	@import url('http://arbolnaranja.com/e-commerceMC/webfontkit-20150326-230131/stylesheet.css');
-	body {
-		background-color:#fff;
-		font-family: 'myriad_set_promedium',sans-serif !important;	
-		margin: 0px; 
-		font-size: 12px;
-		height: 100%;
-	}
-		
-	#placetopay-header { 
-		background-color: #f7f7f7;
-		height: 50px;
-		z-index: 1;
-		border-color: #E0E0E0 !important;
-		box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
-		width: 100%;
-		position: fixed;
-		margin: 0;
-		opacity: 0.9;
-	}
-	.logoMC{
-		width: 960px;
-		height: 50px;
-		margin: auto;
-		background-image: url('http://arbolnaranja.com/e-commerceMC/LOGO-MACCENTER.png');
-		image-rendering: auto;
-		background-repeat: no-repeat;
-		background-position: 10px 5px;
-		background-size: 189px auto;
-		}
-	#placetopay-content { 
-		width: auto;
-		height: 400px;
-/*		width: 500px; */
-		margin-left: auto; 
-		margin-right: auto; 
-		
+<title>Pronto estaremos de vuelta</title>
+<style type="text/css">
+@import url('http://arbolnaranja.com/e-commerceMC/webfontkit-20150326-230131/stylesheet.css');
+body {
+background-color:#fff;
+font-family: 'myriad_set_promedium',sans-serif !important;
+margin: 0px; 
+font-size: 12px;
+height: 100%;
+}
+#placetopay-header { 
+background-color: #f7f7f7;
+height: 50px;
+z-index: 1;
+border-color: #E0E0E0 !important;
+box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
+width: 100%;
+position: fixed;
+margin: 0;
+opacity: 0.9;
+}
+.logoMC{
+width: 960px;
+height: 50px;
+margin: auto;
+background-image: url('http://arbolnaranja.com/e-commerceMC/LOGO-MACCENTER.png');
+image-rendering: auto;
+background-repeat: no-repeat;
+background-position: 10px 5px;
+background-size: 189px auto;
+}
+#placetopay-content { 
+width: auto;
+height: 400px;
+/*	width: 500px; */
+margin-left: auto; 
+margin-right: auto; 
 /*
-		border-radius: 10px; 
-		-moz-border-radius: 10px; 
-		-webkit-border-radius: 10px; 
-		-khtml-border-radius: 10px; 
-		-webkit-box-shadow: 1px 1px 5px 0px #ccc; 
-		box-shadow: 1px 1px 5px 0px #ccc; 
+border-radius: 10px; 
+-moz-border-radius: 10px; 
+-webkit-border-radius: 10px; 
+-khtml-border-radius: 10px; 
+-webkit-box-shadow: 1px 1px 5px 0px #ccc; 
+box-shadow: 1px 1px 5px 0px #ccc; 
 */
-		padding: 30px; 
-	}
-	#placetopay-footer { 
-		margin-top: 20px;
-		border-top: 1px solid #ccc;
-	width: 100%; 
-	margin-left: auto; 
-	margin-right: auto; 
-	text-align: center; 
-	color:#777;
-	font-size: 11px;
-	line-height: 30px;
-	background-color: #F7F7F7 !important;
-	padding-top: 20px;	
-	padding-bottom: 20px;
-	font-family: 'myriad_set_promedium', sans-serif !important;	
-		  position: fixed;
+padding: 30px; 
+}
+#placetopay-footer { 
+margin-top: 20px;
+border-top: 1px solid #ccc;
+width: 100%; 
+margin-left: auto; 
+margin-right: auto; 
+text-align: center; 
+color:#777;
+font-size: 11px;
+line-height: 30px;
+background-color: #F7F7F7 !important;
+padding-top: 20px;
+padding-bottom: 20px;
+font-family: 'myriad_set_promedium', sans-serif !important;
+ position: fixed;
     clear: both;
     bottom: 0;
-	}
-	h3 { 
-		margin-top:150px;
-		font-family: 'myriad_set_probold';
-		text-align: center;		
-		text-transform: none;
-		color:#777777;
-    	width: auto;
-		font-weight: 100 !important;		
-		font-size: 30px; 
-/*		vertical-align: middle;*/
-	
-	}
-	
-	}
-	th.placetopay  {
-		text-align: left;		
-		text-transform: none;
-		padding: 5px 2px 5px 30px !important;
-    	border: 1px solid #CCC;
-    	width: 100%;
-		font-weight: 100 !important;		
-		font-size: 24px; 
-	}
-	td.placetopayheader {
-		font-size: 14pt; 
-		color: #000; 
-		font-weight: bold;
-	}
-
-	#contenido{
-	width: 100%;
-	height:700px;
-	background-image: url('http://arbolnaranja.com/e-commerceMC/TIENDAS-01.jpg');
-	background-size: contain;
-	background-repeat: no-repeat;
-	background-position: center top;
-	margin: auto;
-		background-color:#f7f7f7;
-		border-color: #E0E0E0 !important;
-		box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
-}		
-		
-#placetopay-content p{
-	text-align: center;
-	font-size: 14px;
-	color:#484848;
-	letter-spacing: 0.6px;
-	font-family: 'myriad_set_promedium', sans-serif !important;	
-	
-}		
-		
-		
-		/*ESTILOS MOVIL*/
-@media only screen and (max-width: 700px) {
-	
-	#placetopay-header{
-	background-color: #f7f7f7;
-	height: 190px;
-	z-index: 1;
-	border-color: #E0E0E0 !important;
-	box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);font-family: 'myriad_set_promedium', sans-serif !important;	
-		margin-bottom: 100px;
-
 }
-	.logoMC{
-	   display: block;
-		position: relative;
-		z-index: 2;
-		transition: opacity 0.4s ease-in-out 0s;
-		background-image: url('http://arbolnaranja.com/e-commerceMC/LOGO-MACCENTER.png');
-		image-rendering: auto;
-		background-repeat: no-repeat;
-		background-position: center center;
-		height: 100px !important;
-		width: 500px;
-		background-size: 500px 100px;
-		-ms-transform: translate(10%,50%); /* IE 9 */
-		-webkit-transform: translate(10%,50%); /* Safari */
-		transform: translate(10%,50%);		
-}
-	#placetopay-content {
-    width:90%;
-	min-height: 1200px!important;
-    margin-left: auto;
-	margin-right: auto;
-	margin-top: 100px;
-    border-radius: 3px;
-    padding: 30px;
-	
-}
-	
-	
-	h3 {
+#actualizando {
     text-align: center;
     text-transform: none;
     width: 100%;
-    font-size: 2em;
+    height: 100px;
     margin-top: 150px;
-	color:#777777 !important;
+color:#777777 !important;
+background-image: url('http://arbolnaranja.com/e-commerceMC/actualizando-01.png');
+background-repeat: no-repeat;
+background-position: center center;
+background-size:800px auto;
 }
-	
-	
-	
-	#placetopay-footer p {
-			clear: both;
-			text-align: center;
-		padding: 0px 180px;
-		text-decoration: none;
-	
+}
+}
+th.placetopay  {
+text-align: left;
+text-transform: none;
+padding: 5px 2px 5px 30px !important;
+    border: 1px solid #CCC;
+    width: 100%;
+font-weight: 100 !important;
+font-size: 24px; 
+}
+td.placetopayheader {
+font-size: 14pt; 
+color: #000; 
+font-weight: bold;
+}
+
+#contenido{
+width: 100%;
+height:700px;
+background-image: url('http://arbolnaranja.com/e-commerceMC/TIENDAS-01.jpg');
+background-size: contain;
+background-repeat: no-repeat;
+background-position: center top;
+margin: auto;
+background-color:#f7f7f7;
+border-color: #E0E0E0 !important;
+box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
+}
+#placetopay-content p{
+text-align: center;
+font-size: 14px;
+color:#484848;
+letter-spacing: 0.6px;
+font-family: 'myriad_set_promedium', sans-serif !important;
+}
+/*ESTILOS MOVIL*/
+@media only screen and (max-width: 700px) {
+#placetopay-header{
+background-color: #f7f7f7;
+height: 190px;
+z-index: 1;
+border-color: #E0E0E0 !important;
+box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);font-family: 'myriad_set_promedium', sans-serif !important;
+margin-bottom: 100px;
+
+}
+.logoMC{
+  display: block;
+position: relative;
+z-index: 2;
+transition: opacity 0.4s ease-in-out 0s;
+background-image: url('http://arbolnaranja.com/e-commerceMC/LOGO-MACCENTER.png');
+image-rendering: auto;
+background-repeat: no-repeat;
+background-position: center center;
+height: 100px !important;
+width: 500px;
+background-size: 500px 100px;
+-ms-transform: translate(10%,50%); /* IE 9 */
+-webkit-transform: translate(10%,50%); /* Safari */
+transform: translate(10%,50%);
+}
+#placetopay-content {
+    width:90%;
+min-height: 1200px!important;
+    margin-left: auto;
+margin-right: auto;
+margin-top: 100px;
+    border-radius: 3px;
+    padding: 30px;
+}
+#actualizando {
+    text-align: center;
+    text-transform: none;
+    width: 100%;
+    height: 100px;
+    margin-top: 150px;
+color:#777777 !important;
+background-image: url('http://arbolnaranja.com/e-commerceMC/actualizando-01.png');
+background-repeat: no-repeat;
+background-position: center center;
+background-size: 300px auto;
+}
+#placetopay-footer p {
+clear: both;
+text-align: center;
+padding: 0px 180px;
+text-decoration: none;
  }
-	
-	
-	
-	#placetopay-footer { 
-		margin-top: 20px;
-		border-top: 1px solid #ccc;
-	width: 100%; 
-	margin-left: auto; 
-	margin-right: auto; 
-	text-align: center; 
-	color:#777;
-	font-size: 11px;
-	line-height: 30px;
-	background-color: #F7F7F7 !important;
-	padding-top: 20px;	
-	padding-bottom: 20px;
-	font-family: 'myriad_set_promedium', sans-serif !important;	
-		  position: inherit;
+#placetopay-footer { 
+margin-top: 20px;
+border-top: 1px solid #ccc;
+width: 100%; 
+margin-left: auto; 
+margin-right: auto; 
+text-align: center; 
+color:#777;
+font-size: 11px;
+line-height: 30px;
+background-color: #F7F7F7 !important;
+padding-top: 20px;
+padding-bottom: 20px;
+font-family: 'myriad_set_promedium', sans-serif !important;
+ position: inherit;
     clear: inherit;
     bottom: inherit;
-	}
-
-	
 }
-	</style>
+
+}
+</style>
 </head>
 <body>
 <div id="placetopay-header"><div class="logoMC"></div></div>
 <br />
 <div id="placetopay-content">
-	<h3>Estamos actualizando nuestros productos para que aproveches<br /> al máximo Cyber Monday. Intenta de nuevo en unos minutos.</h3>
-	<p>contáctanos en <a href='mailto:info@mac-center.com'>info@mac-center.com</a></p>
+<div id="actualizando"></div>
+<p>contáctanos en <a href='mailto:info@mac-center.com'>info@mac-center.com</a></p>
 </div>
 <div id="contenido"></div>
 <div id="placetopay-footer"><p>Mac Center Colombia S.A.S. | Nit: 900311581 - 7 | Teléfonos: (+57) 1 7449852 - 7432130 - 7425701, Dirección: Cra. 14 # 81 - 19, oficina 305, Bogotá D.C. | correo electrónico: consultas@mac-center.com / info@mac-center.com</p>
