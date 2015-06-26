34b94a124e786c972a33d2f63fb4fb5f27385050
Manish Runwal <manish@runwalsoft.com>
2015-06-26 21:37:16 +0530

    Corrected Complete Structure of plugin.

--- wp-content/plugins/state-city/state-city.php
+++ wp-content/plugins/state-city/state-city.php
@@ -9,83 +9,123 @@ Domain Path: /languages/
 Version: 0.1.0
 */
  
-	
-add_action('admin_menu', 'my_menu_pages');
-function my_menu_pages(){
- $import_state_city=add_menu_page('Import State City', 'Import State City', 'manage_options', 'import_state_city', 'import_state_city_menu_page' );
-}
+error_reporting(E_ALL ^ E_NOTICE ^ E_STRICT);
+ini_set('display_errors', 1);
+ini_set('display_startup_error', 1);
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
- function import_state_city_menu_page() { 
-	global $wpdb;
+global $wpdb;
+
+add_action('admin_menu', array('States_Cities', 'my_menu_pages'));
+//add_action('admin_init', array('States_Cities', 'init'));
+
+if(!class_exists('States_Cities')){
+class States_Cities{
+	const iZIP = 0 ;
+	const iCITY = 1 ;
+	const iSTATECODE = 2 ;
+	const iSTATE = 3 ;
+	public function __construct(){
+		
+	// Install plugin
+		register_activation_hook( __FILE__, array( $this, 'activate' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+	}
+	public function activate(){
+		require_once("state_install.php");
+			states_cities_install();
+	}
+	public function deactivate(){
+		require_once("state_install.php");
+			states_cities_uninstall();
+	}
+	public function init(){
     wp_enqueue_script('media-upload');  // For WP media uploader
 		wp_enqueue_script('thickbox');  // For WP media uploader
 		wp_enqueue_script('jquery-ui-tabs');  // For admin panel page tabs
 		wp_enqueue_script('jquery-ui-dialog');  // For admin panel popup alerts
   	wp_enqueue_script( 'import_state_city', plugins_url( '/state-city.js', __FILE__ ), array('jquery') );
-  	
-  	wp_enqueue_style('import_state_city', plugins_url( '/state-city.css', __FILE__ )); ?>
- 	
- 
-<div  class="wrap">
-<h2><?php _e('Import State City',''); ?></h2>
-<div id="tabs" class="tabs">
-					<ul>
-    				<li><a class="nav-tab" href="#tabs-1"><?php _e('Upload',''); ?></a></li>
-    				<li><a class="nav-tab" href="#tabs-2"><?php _e('Report',''); ?></a></li>
-        </ul>
-	<div id="tabs-1">
-	<?php
-	if (isset($_POST['submit'])) {
+  	wp_enqueue_style('import_state_city', plugins_url( '/state-city.css', __FILE__ )); 			
 
-		if (is_uploaded_file($_FILES['filename']['tmp_name'])):
+  	States_Cities::renderTable1();
+	}
+	/*
+	//This will be used for saving cities
+	public function saveCities($data){
+		global $wpdb;							
+		
+		$sql = $wpdb->prepare("select count(*) from  ".$wpdb->prefix."cities where state_code ='%d' and city = '%s'", $data[iSTATECODE], $data[iCITY]);
+		$state = $wpdb->get_results ($sql);
+		
+		if($wpdb->num_rows() == 0 ){
+			$rs = $wpdb->insert( $wpdb->prefix . "cities" , 
+																		 array('zip' => $data[iZIP] 
+																					,'city'=> $data[iCITY]
+																					,'state_id'=> $data[iSTATECODE]
+																					),
+																		array('%d','%s','%d'));
+		}
+	}
+	*/
+	//This will be used for saving States
+	public function saveStates($data){
+		global $wpdb;
+		
+		$sql = $wpdb->prepare("select state_code, state_name  from  ". $wpdb->prefix ."states where state_name='%s'", $data[iSTATE]);
+
+		$state = $wpdb->get_results($sql);
+		
+		if($wpdb->num_rows == 0 ){
+		
+			$rs = $wpdb->insert( $wpdb->prefix . "states" , array('state_code' => $data[iSTATECODE] 
+																					,'state_name'=> $data[iSTATE]),
+																		array('%d','%s'));
+		
+			return $wpdb->insert_id;
+		} else{
+			
+			foreach ($state as $s){
+				$stateCode = $s->state_code ;
+				break ;
+			}
+			
+			return $stateCode ; 
+		}
+	
+	}
+	
+	public function parseCSV(){
+		global $wpdb;	
+		echo "test"; 
+		die;
+		/*
+		if (isset($_POST['submit'])) {
+			if (is_uploaded_file($_FILES['filename']['tmp_name'])):
 					//Import uploaded file to Database
 					$handle = fopen($_FILES['filename']['tmp_name'], "r");
 					
 					$i=0;
-					while (($data = fgetcsv($handle, 1000, ",")) !== FALSE):
+					while (($data = fgetcsv($handle)) !== FALSE):
 							$i++;
 								if($i==1) continue;
-							global $wpdb;							
-							$state = $wpdb->get_results ( "select state_name  from  state where state_name='".$data[3]."' " );
-							$city = $wpdb->get_results ( "select zip , city from  city where zip='".$data[0]."' and city='".$data[1]."'" );
-							if(count($state)==0){			
-							$a2 = $wpdb->query("INSERT INTO state (state_code,state_name) VALUES('".$data[2]."','".$data[3]."')");
-							}
-							if(count($city)==0){							
-							$a1 =$wpdb->query("INSERT INTO city(zip,city,state_id) VALUES('".$data[0]."','".$data[1]."','".$data[2]."')");
-							}
+							$id = saveStates($data);
+							saveCities($data);
 					endwhile;
 					fclose($handle);
-						if (is_uploaded_file($_FILES['filename']['tmp_name'])) {
-									echo "<h2>" . "File ". $_FILES['filename']['name'] ." uploaded successfully." . "</h2>";
-						}
+					if (is_uploaded_file($_FILES['filename']['tmp_name'])) {
+								echo "<h2>" . "File ". $_FILES['filename']['name'] ." uploaded successfully." . "</h2>";
+					}
 			endif;	//if file uploaded properly
-			
-		}else { ?>
-		
-				
-				<br/>
-				<form enctype='multipart/form-data'  method='post'>
-
-				<label> Select File to import:<br /></label>
-
-				<input size='50' type='file' name='filename'><br /><br />
-
-				<input type='submit' class='button-primary' name='submit' value='Upload'>
-				
-				</form>
-	<?php } ?>
-	</div> <!--tab-1-->
-	<div id="tabs-2">
-		<h2>State City Table</h2>
-		<?php
-		if( ! class_exists( 'WP_List_Table' ) ) {
-    require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 		}
-		function get_state_city_data(){
+		*/
+	}
+		
+	// Showing all the states and Cities
+	public function listData(){
 			global $wpdb;
 			$rows = array();
-			$result = $wpdb->get_results ( "select c.zip,c.city,c.state_id, s.state_name from wp_cities c , wp_states s  
+			$result = $wpdb->get_results ( "select c.zip,c.city,c.state_id, s.state_name from ".$wpdb->prefix."cities c , 
+																				".$wpdb->prefix."states s  
 																				where c.state_id=s.state_code order by state_name,city " );
 				foreach($result as $val){
 					$rows[$val->zip] = array(
@@ -96,93 +136,58 @@ function my_menu_pages(){
 					);
 				}
 				//print_r($rows);
-				return $rows;
-		}
-		class My_List_Table extends WP_List_Table {
-						
-			function column_cb( $item ) {
-			$array = get_state_city_data();
-			foreach($array as $key=>$value){
-			//$zip=$key;
-			}
-			//$zip=reset($array);
-			//print_r($zip);
-					return sprintf(
-						'<input type="checkbox" name="bulk-delete[]" value="'.$zip['zip'].'" />', 
-						$this->_args['singular'], 
-						$item['ID']
-							);
-			}
-			function get_columns(){
-					$columns = array(
-						'cb'           => '<input type="checkbox" />',
-						'zip' => 'Zip',
-						'city'    => 'City',
-						'state_code' => 'State Code',
-						'state_name' => 'State Name'
-					);
-					return $columns;
-			}
-			
-			function prepare_items() {
-					$columns = $this->get_columns();
-					$hidden = array();
-					$sortable = array();
-					$this->_column_headers = array($columns, $hidden, $sortable);
-					$per_page = 50;
-					$current_page = $this->get_pagenum();
-					$total_items = count(get_state_city_data());
-					// only ncessary because we have sample data
-					$this->found_data = array_slice(get_state_city_data(),(($current_page-1)*$per_page),$per_page);
-					$this->set_pagination_args( array(
-								'total_items' => $total_items,                  //WE have to calculate the total number of items
-								'per_page'    => $per_page                     //WE have to determine how many items to show on a page
-					) );
-					$this->process_bulk_action();
-					?>
-					<form method="post">
-					<input type="hidden" name="page" value="my_list_test" />
-					<?php $this->search_box('search', 'search_id'); ?>
-					</form>
-					<?php
-					$this->items = $this->found_data;
-			}	
-			function process_bulk_action() {        
-      
-        if( 'delete'===$this->current_action() ) {
-           foreach($_GET['bulk-delete'] as $id) {                             
-                multiple_delete($id);
-            }
-        }        
-			}
-			function column_default( $item, $column_name ) {
-					switch( $column_name ) { 
-						case 'cb':
-						case 'zip':
-						case 'city':
-						case 'state_code':
-						case 'state_name':
-							return $item[ $column_name ];
-						default:
-							return print_r( $item, true ) ; //Show the whole array for troubleshooting purposes
-					}
-			}
-			function get_bulk_actions() {
-				$actions = array(
-				'delete'    => 'Delete'
-				);
-				return $actions;
-			}
-			
-		}
-		$myListTable = new My_List_Table();
+				return $rows;	
+	}
+	
+	//used for bluck delete
+	public function bulkDelete(){
+	
+	}
+
+	//Rendering Table
+	public function renderTable1(){ 
+
+	?>
 		
+			<div  class="wrap">
+			<h2><?php _e('Import State City',''); ?></h2>
+			<div id="tabs" class="tabs">
+								<ul>
+									<li><a class="nav-tab" href="#tabs-1"><?php _e('Upload',''); ?></a></li>
+									<li><a class="nav-tab" href="#tabs-2"><?php _e('Report',''); ?></a></li>
+							</ul>
+				<div id="tabs-1">	
+					<br/>
+						<form enctype='multipart/form-data'  method='post'>
+						<input type='hidden' name='action' value="importcsv">
+						<label> Select File to import:<br /></label>
+						<input size='50' type='file' name='filename'><br /><br />
+						<input type='submit' class='button-primary' name='submit' value='Upload'>
+					</form>					
+				</div>
+				<div id="tabs-2">
+					<h2>State City Table</h2>		
+					<?php 
+					echo States_Cities::listData();
+					?>
+				</div>
+			</div> <!--wrap-->
+	<?php }
 	
-    $myListTable->prepare_items(); 
-    $myListTable->display(); 
-		?>
-	</div> <!--tabs-2-->
-	</div> <!--tabs-->
-	</div> <!--wrap-->
+	//Form Menu
+	function my_menu_pages(){
+		$import_state_city	=	add_menu_page('States Cities', 'States Cities', 'manage_options', 
+		'states_cities', array('States_Cities', 'init') );
+	}
+
 	
-<?php } ?>
+		//Create menu
+		function menu_list(){
+
+		}
+	} // Class
+	$ob = new States_Cities ;
+	add_action( 'admin_post_importcsv', array( 'States_Cities','parseCSV' ) );	
+}// If 
+
+
